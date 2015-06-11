0d8dc2c008b880420e8356e43866f80b7e64c591
marshallj <marshallj88@gmail.com>
2015-06-11 13:36:58 -0400

    Added more robust comments, legislator photos and some formatting.

--- app/assets/javascripts/geolegislate.js
+++ app/assets/javascripts/geolegislate.js
@@ -1,14 +1,9 @@
-var reps = {};
 var autocomplete, address, status;
 
 window.onload = function() {
   status = "Fetching representatives for your current location...";
   document.getElementById("status").innerHTML = status;
 }
-
-// This example displays an address form, using the autocomplete feature
-// of the Google Places API to help users fill in the information.
-
 
 function initialize() {
   // Create the autocomplete object, restricting the search
@@ -24,14 +19,16 @@ function initialize() {
   });
 }
 
-
+// Creates latitude and longitude coordinates from the address entered by the user,
+// and wraps them in a json object. The object is passed in a call to get the legislators
+// for the address.
 function getCoordForAddress() {
   var geocoder = new google.maps.Geocoder().geocode( { 'address' : address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       var latitude = results[0].geometry.location.lat();
       var longitude = results[0].geometry.location.lng();
       var position = { "coords" :  {"latitude" : latitude, "longitude" : longitude } };
-      getLegislatures(position);
+      getLegislators(position);
     }
   });
 }
@@ -52,23 +49,23 @@ function geolocate() {
   }
 }
 
-//geolocation
+//Geolocation used to get the legislators for the users current location.
 function getLocation() {
     if (navigator.geolocation) {
         status = "N.C. legislators for your current location:";
-        navigator.geolocation.getCurrentPosition(getLegislatures);
+        navigator.geolocation.getCurrentPosition(getLegislators);
     } else {
       status = "Geolocation is not supported by this browser.";
       document.getElementById("status").innerHTML = status;
     }
 }
 
-function getLegislatures(position) {
-
+// Makes a call to the open states API to retrieve the N.C. legislators for the coordinates passed in.
+function getLegislators(position) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
-        reps = JSON.parse(xhr.responseText);
+        var reps = JSON.parse(xhr.responseText);
         displayReps(reps);
         document.getElementById("status").innerHTML = status;
         }
@@ -79,10 +76,10 @@ function getLegislatures(position) {
     //console.log(xhr.statusText);
 }
 
+//Formats and displays the legislators information on the page.
 function displayReps(representatives) {
 
     var repNum = "1";
-    var myRep = "";
     var img = "";
     var website = "";
     var name = "";
@@ -90,19 +87,18 @@ function displayReps(representatives) {
     var phone = "";
     var email = "";
     var address = "";
+    var myRep = "houseRep";
     for (var rep in representatives) {
-
-       myRep = "myRep" + repNum;
-       img = representatives[rep].photo_url;
-       website = representatives[rep].url;
+       img = (representatives[rep].photo_url).replace("/hiRes", "");
+       website = representatives[rep].sources[0].url;
        name = representatives[rep].full_name;
        party = representatives[rep].party;
        phone = representatives[rep].offices[0].phone;
        email = representatives[rep].email;
-       address = representatives[rep].offices[0].address;
-       var ul = "<ul><li><a href=" + website + ">" + name +"</a></li><li>" + party + "</li><li><a href=\"tel:" + phone + "\">" + phone + "</a></li><li><a href=\"mailto:" + email + "\">" + email + "</a></li><li>" + address + "</li></ul><br>";
+       address = (representatives[rep].offices[1].address).replace(/\r\n|\r|\n/g, "<br>");
+       var ul = "<ul><li><img src=\"" + img + "\" style=\"height:235px;\"></li><li><a href=" + website + ">" + name +"</a></li><li>" + party + "</li><li>" + address + "</li><li><a href=\"tel:" + phone + "\">" + phone + "</a></li><li><a href=\"mailto:" + email + "\">" + email + "</a></li></ul><br>";
        document.getElementById(myRep).innerHTML = ul;
-       repNum = "2";
+       myRep = "senateRep";
    }
 }
 
